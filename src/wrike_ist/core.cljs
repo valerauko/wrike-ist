893ae91c40bc8c8f080a76fe30ade48e9aea7764
Vale <valerauko@gmail.com>
2021-05-27 00:06:19 +0900

    Try using cljs-http

--- src/wrike_ist/core.cljs
+++ src/wrike_ist/core.cljs
@@ -7,8 +7,9 @@
   [pr-obj]
   (let [body (.-body pr-obj)
         url (.-html_url pr-obj)]
-    (when-let [[_ id] (re-find #"open\.htm\?id=(\d+)" body)]
+    (when-let [[perm id] (re-find #"https://www.wrike.com/open\.htm\?id=(\d+)" body)]
       {:task-id id
+       :permalink perm
        :pr-url url})))
 
 (defn main
@@ -16,8 +17,8 @@
   (try
     (let [payload (.-payload (.-context github))]
       (if-let [pr (.-pull_request payload)]
-        (if-let [{:keys [task-id pr-url]} (extract-details pr)]
-          (wrike/link-pr task-id pr-url)
+        (if-let [details (extract-details pr)]
+          (wrike/link-pr details)
           (js/console.log "Not task link in PR text"))
         (js/console.log "No pull_request in payload")))
     (catch js/Error ex
--- src/wrike_ist/wrike.cljs
+++ src/wrike_ist/wrike.cljs
@@ -1,11 +1,22 @@
 (ns wrike-ist.wrike
-  (:require [wrike-ist.http :as http]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
+
+(defn- wrike-token
+  []
+  (.-WRIKE_TOKEN (.-env js/process)))
 
 (def link-badge
   "<span style=\"background-color: #966AF0\">Pull request:</span> ")
 
 (defn link-pr
-  [task-id pr-url]
-  (http/post
-   (str "https://www.wrike.com/api/v4/tasks/" task-id "/comments")
-   {:params {:text (str link-badge pr-url)}}))
+  [{:keys [task-id pr-url permalink]}]
+  (go
+   (let [response (<! (http/get "https://wrike.com/api/v4/tasks"
+                                {:query-params {:permalink permalink}
+                                 :headers {:Authorization
+                                           (str "bearer " (wrike-token))}}))]
+     (js/console.log (:body response)))))
+   ; (str "tasks/" task-id "/comments")))
+   ; {:params {:text (str link-badge pr-url)}}))
