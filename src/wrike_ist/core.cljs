9c99e584940a2135261c281fd4b7ad34482dbff3
Vale <valerauko@gmail.com>
2021-05-26 20:14:25 +0900

    Add initial debug code

--- src/wrike_ist/core.cljs
+++ src/wrike_ist/core.cljs
@@ -1,4 +1,24 @@
-(ns wrike-ist.core)
+(ns wrike-ist.core
+  (:require ["@actions/core" :as core]
+            ["@actions/github" :as github]))
+
+(defn extract-details
+  [pr-obj]
+  (let [body (.-body pr-obj)
+        url (.-html_url pr-obj)]
+    (when-let [[_ id] (re-find #"open\.htm\?id=(\d+)" body)]
+      {:task-id id
+       :pr-url url})))
 
 (defn main
-  [])
+  []
+  (try
+    (some->> (.-context github)
+             (.-payload)
+             (.-pull_request)
+             (extract-details)
+             (js/console.log))
+    (catch js/Error ex
+      (core/setFailed (.-message ex)))))
+
+(main)
