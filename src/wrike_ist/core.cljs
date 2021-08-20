9a2130b5efab1c026a7c5e1131a936ac22e25e51
ERDOS Balint <valerauko@gmail.com>
2021-08-20 15:23:07 +0900

    Fix error when PR body is empty
    Fixes #8

--- src/wrike_ist/core.cljs
+++ src/wrike_ist/core.cljs
@@ -5,7 +5,7 @@
 
 (defn extract-details
   [pr-obj]
-  (let [body (.-body pr-obj)]
+  (when-let [body (.-body pr-obj)]
     (when-let [[perm] (re-find #"https://www.wrike.com/open\.htm\?id=(\d+)" body)]
       {:state (cond
                 ^boolean (.-merged pr-obj) :merged
