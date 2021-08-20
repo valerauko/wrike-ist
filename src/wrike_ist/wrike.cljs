33d902cf5821fbf15d0f9d2d1ba002212be3ee8e
ERDOS Balint <valerauko@gmail.com>
2021-08-20 15:37:34 +0900

    Debug token

--- src/wrike_ist/wrike.cljs
+++ src/wrike_ist/wrike.cljs
@@ -21,7 +21,7 @@
   [permalink]
   (let [uri (str "https://www.wrike.com/api/v4/tasks?permalink="
                  (js/encodeURIComponent permalink))]
-    (js/console.log "Why is it empty?" (headers))
+    (js/console.log "Why is it empty?" (str "\"" (wrike-token) "\""))
     (.then
      (http/get uri {:headers (headers)})
      (fn [response]
