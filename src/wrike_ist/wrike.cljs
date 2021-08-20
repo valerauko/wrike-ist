c41a145558179aaaaad0e13300c413b7f5afda5e
ERDOS Balint <valerauko@gmail.com>
2021-08-20 15:34:39 +0900

    Debug headers

--- src/wrike_ist/wrike.cljs
+++ src/wrike_ist/wrike.cljs
@@ -21,6 +21,7 @@
   [permalink]
   (let [uri (str "https://www.wrike.com/api/v4/tasks?permalink="
                  (js/encodeURIComponent permalink))]
+    (js/console.log "Why is it empty?" (headers))
     (.then
      (http/get uri {:headers (headers)})
      (fn [response]
