a896fc80f55182004d3fc7c54c32b58e786240f7
ERDOS Balint <valerauko@gmail.com>
2023-05-30 18:29:33 +0900

    Test case for missing .body

--- test/wrike_ist/core_test.cljs
+++ test/wrike_ist/core_test.cljs
@@ -1,8 +1,8 @@
 (ns wrike-ist.core-test
-  (:require [cljs.test :refer-macros [deftest is testing run-tests]]))
+  (:require [cljs.test :refer-macros [deftest is testing run-tests]]
+            [wrike-ist.core :refer [extract-details]]))
 
-(deftest passing-test
-  (is (= 1 1)))
-
-(deftest failing-test
-  (is (= 1 2)))
+(deftest extract-details-test
+  (testing "No .body in payload"
+    (let [payload (clj->js {})]
+      (is (= nil (extract-details payload))))))
